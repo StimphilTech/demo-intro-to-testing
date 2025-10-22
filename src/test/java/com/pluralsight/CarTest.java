e8fe7075619f20331b5215e0f9a4230c245732f2
Monalisa Stimphil <stimphiltechnologist@gmail.com>
2025-10-22 09:04:17 -0400

    cartest public void test

---
+++ src/test/java/com/pluralsight/CarTest.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    public void accerlerate_NormalSpeedChangeSpeedIncreased(){
+
+
+    }
+@Test
+ public void brake_SpeedChangeGreaterThanActualSpeed_SpeedIsZero(){
+
+
+}
+}
